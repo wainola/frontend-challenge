4c1452e21282c29911ae3c263a7803a1649a519b
Nicolás Riquelme <nrriquelme@gmail.com>
2018-10-22 10:31:56 -0300

    adding some comment

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,9 @@
-import { combineReducers } from 'redux'
-import FinCardReducer from './FinCards/FinCardsReducer'
-import FinFormReducer from './Form/FormReducer'
+import { combineReducers } from "redux";
+import FinCardReducer from "./FinCards/FinCardsReducer";
+import FinFormReducer from "./Form/FormReducer";
 
+// this is the root reducer
 export default combineReducers({
   cards: FinCardReducer,
   card_to_patch: FinFormReducer
-})
+});
